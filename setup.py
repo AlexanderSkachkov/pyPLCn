ddb4eecbe06ab04779fb60183fd721f0b5556604
AlexanderSkachkov <lex.skachkov.it@gmail.com>
2020-12-02 18:52:34 +0200

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyPLCn',
-    version='1.0.0',
+    version='1.0.1',
     packages=[''],
     url='https://github.com/AlexanderSkachkov/PyPLCn',
     license='MIT',
